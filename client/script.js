41932b549b84d9e39ee89911e33bf009bc75dcbf
Marlon Aragon <aragonmarlon1@gmail.com>
2023-06-28 01:40:01 +0800

    update url for vercel

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   //fetch datafrom server -> bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-tuit.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
